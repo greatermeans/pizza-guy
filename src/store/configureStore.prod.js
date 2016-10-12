7a762e41f24aadf708d43f773f1f77497d4ca8fe
George Akouri <george@philosophie.is>
2016-10-11 22:09:36 -0400

    store fix

--- src/store/configureStore.prod.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import rootReducer from '../reducers'
-import promise from 'redux-promise'
-
-const enhancer = applyMiddleware(promise)
-
-export default function configureStore(initialState) {
-  return createStore(rootReducer, initialState, enhancer)
-}
