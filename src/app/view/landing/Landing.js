c576631a0be9850e014b9fc5de843078c101c8cb
leearaneta <lee.araneta@gmail.com>
2016-11-14 23:08:33 -0500

    this sucks

--- src/app/view/landing/Landing.js
+++ src/app/view/landing/Landing.js
@@ -12,19 +12,11 @@ class Landing extends Component {
   render() {
     return (
       <div>
-        <Paper style={styles.mapPaper} zDepth={1} rounded={false}>
-          Hello WOrld
-          <div style={styles.map}>
-            <GoogleMap
-              center={{lat: 59.6205245, lng: 17.843808}}
-              zoom={12}
-            >
-              <Marker
-                lat={59.6205245}
-                lng={17.843808}
-              />
-            </GoogleMap>
-          </div>
+        <Paper style={styles.paper} zDepth={1} rounded={false}>
+          Hello World
+          <GoogleMap styles={styles.map} center={{lat: 59.6205245, lng: 17.843808}} zoom={12}>
+            <Marker lat={59.6205245} lng={17.843808} />
+          </GoogleMap>
         </Paper>
       </div>
     )
@@ -33,14 +25,12 @@ class Landing extends Component {
 
 const styles = {
   map: {
-    margin: 20,
-    width: 200,
-    height: 'auto'
+
   },
   nothing: {
 
   },
-  mapPaper: {
+  paper: {
     height: 300,
     width: 300,
     textAlign: 'center',
