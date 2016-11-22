664b8cdeb81239d73a75a1d4ec7d5b48e78a8e57
George Akouri <george@philosophie.is>
2016-11-21 19:35:55 -0500

    returning old files

---
+++ src/app/view/cart/CartView.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { List, ListItem, Paper} from 'material-ui'
+import ContentRemoveCircle from 'material-ui/svg-icons/content/remove-circle'
+import EditorModeEdit from 'material-ui/svg-icons/editor/mode-edit'
+
+class CartView extends Component {
+
+  render() {
+    let { cart, items, types, } = this.props
+
+    return (
+      <Paper style={styles.menu} zDepth={3} rounded >
+        <List>
+        {
+          Object.keys(cart).map(item => {
+            let { instructions, quantity, type, } = item
+            return (
+              <ListItem
+                leftAvatar={<ContentRemoveCircle/>}
+                rightIconButton={<EditorModeEdit/>}
+                primaryText={items[item].name}
+                secondaryText={
+                  <p>
+                    <span style={{color: darkBlack}}>Brunch this weekend?</span><br />
+                    I&apos;ll be in your neighborhood doing errands this weekend. Do you want to grab brunch?
+                  </p>
+                }
+                secondaryTextLines={2}
+              />
+            )
+          })
+        }
+
+        <Divider inset={true} />
+      </List>
+      </Paper>
+    )
+  }
+}
+
+const styles = {
+  container: {
+    display: 'flex'
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    cart: state.cart,
+    items: state.items,
+    types: state.types
+  }
+}
+
+export default connect(mapStateToProps, null)(CartView)
